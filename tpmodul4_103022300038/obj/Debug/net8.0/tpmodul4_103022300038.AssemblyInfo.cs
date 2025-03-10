f67ee7d4eddd891d78e6abcac09ccc466a47c91a
alfianrizky <alfianrizkysabiann@gmail.com>
2025-03-10 23:00:48 +0700

    menambahkan implementasi dengan state-based construction

--- tpmodul4_103022300038/obj/Debug/net8.0/tpmodul4_103022300038.AssemblyInfo.cs
+++ tpmodul4_103022300038/obj/Debug/net8.0/tpmodul4_103022300038.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_103022300038")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4fc7aa7b604a2d1d89add277081cc17cff18164")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_103022300038")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_103022300038")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
